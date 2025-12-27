ff5b8b859fd230edba2124c9d45dca968864bbd1
smly <smly@users.noreply.github.com>
2025-12-27 14:52:49 +0900

    fix: s/tonpuusen/tonpusen/

--- riichienv/src/riichienv/game_mode.py
+++ riichienv/src/riichienv/game_mode.py
@@ -3,7 +3,7 @@ from enum import IntEnum
 
 class GameType(IntEnum):
     YON_IKKYOKU = 0  # 4-Player One Kyoku
-    YON_TONPUSEN = 1  # 4-Player East (Tonpuusen)
+    YON_TONPUSEN = 1  # 4-Player East (Tonpusen)
     YON_HANCHAN = 2  # 4-Player South (Hanchan)
     SAN_IKKYOKU = 3  # 3-Player One Kyoku
     SAN_TONPUSEN = 4  # 3-Player East
